b422b31a3eb6e477a18765b4a0b6828bdeb1d269
Berkay Yazıcı <berkayyazici27@gmail.com>
2024-08-09 01:00:03 +0300

    fix

---
+++ UrlShortener.Application/UrlService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Application;
+using UrlShortener.Application.Contracts;
+using UrlShortener.Domain.Url;
+
+namespace UrlShortener.Application
+{
+    public class UrlService : IUrlRepository
+    {
+        public List<Url> AllUrls()
+        {
+            List<Url> urls = new List<Url>();   
+
+            Url url1 = new Url()
+            {
+                ID = Guid.NewGuid(),
+                ShortUrl = "https://testURL/22",
+                LongUrl = "https://tU/22"
+            };
+
+
+            Url url2 = new Url()
+            {
+                ID = Guid.NewGuid(),
+                ShortUrl = "https://testURL/33",
+                LongUrl = "https://tU/33"
+            };
+
+
+            Url url3 = new Url()
+            {
+                ID = Guid.NewGuid(),
+                ShortUrl = "https://testURL/44",
+                LongUrl = "https://tU/44"
+            };
+
+            urls.Add(url1);
+            urls.Add(url2);
+            urls.Add(url3);
+
+            return urls;
+        }
+
+        public string GetShortUrl(string longUrl, string headerLink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLongUrl(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UrlShortener.EntityFrameworkCore/Migrations/20240805181349_initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LongUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Urls",
+                columns: new[] { "ID", "LongUrl", "ShortUrl" },
+                values: new object[] { new Guid("47fef85d-158b-4d94-9cbb-f3e88ff4e0b0"), "emre", "berkay" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
