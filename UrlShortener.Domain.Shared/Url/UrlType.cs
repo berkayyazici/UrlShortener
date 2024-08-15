cea0890e9c5a92c19ace893b2d19bdaf5000594b
Berkay Yazıcı <berkayyazici27@gmail.com>
2024-08-16 00:15:24 +0300

    added Domain.Shared

---
+++ UrlShortener.Domain.Shared/Url/UrlType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Domain.Shared.Url
+{
+    enum UrlType
+    {
+        Undefined,
+        Shopping,
+        Education
+    }
+}
